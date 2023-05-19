70b47d5b64204a4b855480ab09a294b7cc92c4f1
tsertic <tsertic5@gmail.com>
2023-05-19 12:54:20 +0200

    added styles to ColorBpx, added copytoclipboard for copy colors on click

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./ColorBox.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 const ColorBox = ({ background, name }) => {
   return (
-    <div className="ColorBox" style={{ background: background }}>
-      <span>{name}</span>
-      <span>MORE</span>
-    </div>
+    <CopyToClipboard text={background}>
+      <div className="ColorBox" style={{ background }}>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
+      </div>
+    </CopyToClipboard>
   );
 };
 
