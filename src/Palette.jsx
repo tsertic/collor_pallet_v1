dce1f44b82bd59bbee85c3e049c3886e0a49ebfd
tsertic <tsertic5@gmail.com>
2023-05-19 11:04:41 +0200

    created palette main component,reset css

---
+++ src/Palette.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Palette = ({ palette }) => {
+  return (
+    <div className="palette">
+      {/* navbar goes here */}
+      <div className="palette-color">{/* bunch of color boxes */}</div>
+      {/* footer eventualy */}
+    </div>
+  );
+};
+
+export default Palette;
